9158aeff7ed50bc10b67961d387ba0255a200461
赵天峰 <zhaotianfeng@zhaotianfengdeMacBook-Pro.local>
2022-11-18 17:44:22 +0000

    fix: refactor project structure and reload the ScreenTouch script

---
+++ BeeInTheField/Assets/Scripts/ScreenTouch.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenTouch : MonoBehaviour
+
+{
+
+    Vector3 mousePositionOnScreen;//moust position
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get the moust position
+        mousePositionOnScreen = Input.mousePosition;
+
+        Debug.Log(mousePositionOnScreen);
+
+
+    }
+}
