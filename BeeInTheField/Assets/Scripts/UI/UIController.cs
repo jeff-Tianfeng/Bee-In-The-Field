442e7016d2d2d51ac6e6a082e6683bb34b5974d9
赵天峰 <zhaotianfeng@zhaotianfengdeMacBook-Pro.local>
2022-12-05 15:40:37 +0000

    feat: add time count down UI

--- BeeInTheField/Assets/Scripts/UI/UIController.cs
+++ BeeInTheField/Assets/Scripts/UI/UIController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
@@ -8,18 +9,51 @@ public class UIController : MonoBehaviour
     private GameObject instance;
     private float t1;
     private float t2;
+    private float gameTime = 120;
+    private float intervaletime=1;
+    private Text text;
+    private Text time;
     // Start is called before the first frame update
     void Start()
     {
         instance = (GameObject)Instantiate(obj);
+        text = GameObject.Find("Canvas/Text").GetComponent<Text>();
+        time = GameObject.Find("Canvas/Time").GetComponent<Text>();
+        time.text = string.Format("{0:00}:{1:00}", (int)gameTime / 60, (float)gameTime % 60);
         t1 = Time.fixedTime;
     }
     void Update()
     {
-        t2 = Time.fixedTime;//一直在变化的时间，2s,3s.....
-          if(t2 -t1 >=2)
+        TimeCountDown();
+        t2 = Time.fixedTime;
+          if(t2 - t1 >=4)
         {
-           Destroy(this.instance);
+            text.text = "";
         }
+
+
     }
+
+    private void TimeCountDown(){
+
+        int M = (int)(gameTime / 60);
+        float S = gameTime % 60;
+            if (gameTime > 0)
+            {
+                intervaletime -= Time.deltaTime;
+                if (intervaletime <= 0)
+                {
+                    intervaletime += 1;
+                    gameTime--;
+                    time.text = string.Format("{0:00}:{1:00}", M, S);
+
+                }
+            }
+            if (gameTime <= 0)
+            {
+                gameTime = 6;
+            }
+        
+    }
+
 }
